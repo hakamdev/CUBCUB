1589a825d52fd9d03eead0733a0939dbf435633d
Elkebir Hakam <ehakam@e2r9p8.1337.ma>
2020-10-29 19:47:30 +0100

    Another bug fixed! :)

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SRC  =	cubengine/*.c cubutils/*.c
 name =	cub3d
 ARG	 =	map.cub
 FLGS =	-Wall -Wextra -Werror
-DBUG =	-g3 -fsanitize=address
+DBUG =	-g3 -fsanitize=undefined
 OPT  =	-Ofast
 
 all: $(name)
@@ -11,7 +11,7 @@ all: $(name)
 $(name):
 	@echo "Compiling..."
 	@#clang $(SRC) -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11 -lbsd -lm -o $(name) $(OPT)
-	@gcc -I minilibx -lmlx -framework OpenGL -framework AppKit $(SRC) -o $(name) $(OPT)
+	@gcc -I minilibx -lmlx -framework OpenGL -framework AppKit $(SRC) -o $(name)
 clean:
 	@echo "Cleaning..."
 	@rm -rf $(name)
--- cubengine/cub_screenshot.c
+++ cubengine/cub_screenshot.c
@@ -10,7 +10,7 @@ int		take_screenshot(t_cub *cub)
 	int fd;
 
 	fd = open("ss.bmp", O_RDWR);
-	g_end_splsh = TRUE;
+	//g_end_splsh = TRUE;
 	event_game_loop(cub);
 	bmp.info.bpp = 24;
 	uint32_t wipixel = ((WIN_WIDTH * bmp.info.bpp + 31) / 32) *4;
@@ -30,18 +30,39 @@ int		take_screenshot(t_cub *cub)
 	memcpy(headerdata + 26, &bmp.info.planes, 2);
 	memcpy(headerdata + 28, &bmp.info.bpp, 2);
 
-	for (size_t i = (WIN_WIDTH * WIN_HEIGHT) - 1, j = 55; i >= 0 && j < imgsize; i--)
+	long long		y;
+	long long		x;
+	long long		j;
+	unsigned int	clr_hex;
+	t_color		clr_rgb;
+	
+	j = 55;
+	
+	y = WIN_HEIGHT - 1;
+	while (y >= 0)
 	{
-		const t_color clr = color_hex_to_rgb(cub->cnvs.data[i]);
-		// unsigned char r, g, b;
-		// int clr = cub->cnvs.data[i];
-		// r = clr / POW(256);
-		// g = (clr - (r * POW(256))) / 256;
-		// b = clr - (r * POW(256) + g * 256);
-		memset(headerdata + j++, clr.g, 1); // g
-		memset(headerdata + j++, clr.r, 1); // r
-		memset(headerdata + j++, clr.b, 1); // b
+		x = 0;
+		while (x < WIN_WIDTH && j < imgsize)
+		{
+			size_t index = (y * WIN_WIDTH) + x;
+			clr_hex = cub->cnvs.data[index];
+			clr_rgb = color_hex_to_rgb(clr_hex);
+			memset(headerdata + j++, clr_rgb.g, 1); // g
+			memset(headerdata + j++, clr_rgb.r, 1); // r
+			memset(headerdata + j++, clr_rgb.b, 1); // b
+			x++;
+		}
+		y--;
 	}
+	
+
+	// for (size_t i = (WIN_WIDTH * WIN_HEIGHT) - 1, j = 55; i >= 0 && j < imgsize; i--)
+	// {
+	// 	const t_color clr = color_hex_to_rgb(cub->cnvs.data[i]);
+	// 	memset(headerdata + j++, clr.g, 1); // g
+	// 	memset(headerdata + j++, clr.r, 1); // r
+	// 	memset(headerdata + j++, clr.b, 1); // b
+	// }
 	
 	write(fd, headerdata, imgsize + 54);
 	close(fd);
@@ -53,7 +74,7 @@ t_color		color_hex_to_rgb(unsigned int color)
 	t_color		out_clr;
 
 	out_clr.r = color / POW(256);
-	out_clr.g = (color - (r * POW(256))) / 256;
-	out_clr.b = color - (r * POW(256) + g * 256);
+	out_clr.g = (color - (out_clr.r * POW(256))) / 256;
+	out_clr.b = color - (out_clr.r * POW(256) + out_clr.g * 256);
 	return (out_clr);
 }
